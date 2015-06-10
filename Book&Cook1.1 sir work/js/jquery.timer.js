a50f29d8175799a0cfd1f27213df173c0c04baa0
Muzamel <muzamel.saleem@gmail.com>
2015-06-10 11:39:05 +0500

    updated work

---
+++ Book&Cook1.1 sir work/js/jquery.timer.js
@@ -0,0 +1,63 @@
+
+
+                $('#timepicker1').timepicker({
+                    template: false,
+                    showInputs: false,
+                    minuteStep: 5
+                });
+
+                $('#timepicker2').timepicker({
+                    template: false,
+                    showInputs: false,
+                    minuteStep: 5
+                });
+                
+                 $('#timepicker3').timepicker({
+                    template: false,
+                    showInputs: false,
+                    minuteStep: 5
+                });
+
+                $('#timepicker4').timepicker({
+                    template: false,
+                    showInputs: false,
+                    minuteStep: 5
+                });
+                
+                
+                 $('#timepicker5').timepicker({
+                    template: false,
+                    showInputs: false,
+                    minuteStep: 5
+                });
+
+                $('#timepicker6').timepicker({
+                    template: false,
+                    showInputs: false,
+                    minuteStep: 5
+                });
+
+                 $('#timepicker7').timepicker({
+                    template: false,
+                    showInputs: false,
+                    minuteStep: 5
+                });
+
+                $('#timepicker8').timepicker({
+                    template: false,
+                    showInputs: false,
+                    minuteStep: 5
+                });
+
+
+
+                
+
+
+
+
+
+
+
+
+         
